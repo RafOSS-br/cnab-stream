cfb687d4ac11e39728dae56910c761bd33d05160
Rafael R. Marques <rrmmbmg903@gmail.com>
2024-09-26 00:01:03 -0300

    test: add benchmark tests

--- cnab/processor_test.go
+++ cnab/processor_test.go
@@ -141,3 +141,101 @@ func TestProcessor_PackRecord(t *testing.T) {
 		t.Errorf("Expected record %s, got %s", expectedRecord, string(record))
 	}
 }
+
+// Benchmark Tests
+
+func BenchmarkProcessor_ParseRecord(b *testing.B) {
+	ctx := context.Background()
+	p := NewProcessor()
+
+	specJSON := `
+	{
+		"fields": [
+			{
+				"name": "bank_code",
+				"type": "int",
+				"start": 1,
+				"length": 3
+			},
+			{
+				"name": "payment_date",
+				"type": "date",
+				"start": 4,
+				"length": 8,
+				"format": "YYYYMMDD"
+			},
+			{
+				"name": "payment_amount",
+				"type": "float",
+				"start": 12,
+				"length": 10,
+				"decimal": 2
+			}
+		]
+	}`
+
+	err := p.LoadSpec(ctx, strings.NewReader(specJSON))
+	if err != nil {
+		b.Fatalf("Failed to load spec: %v", err)
+	}
+
+	record := []byte("001202101011234567890")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := p.ParseRecord(ctx, record)
+		if err != nil {
+			b.Fatalf("Failed to parse record: %v", err)
+		}
+	}
+}
+
+func BenchmarkProcessor_PackRecord(b *testing.B) {
+	ctx := context.Background()
+	p := NewProcessor()
+
+	specJSON := `
+	{
+		"fields": [
+			{
+				"name": "bank_code",
+				"type": "int",
+				"start": 1,
+				"length": 3
+			},
+			{
+				"name": "payment_date",
+				"type": "date",
+				"start": 4,
+				"length": 8,
+				"format": "YYYYMMDD"
+			},
+			{
+				"name": "payment_amount",
+				"type": "float",
+				"start": 12,
+				"length": 10,
+				"decimal": 2
+			}
+		]
+	}`
+
+	err := p.LoadSpec(ctx, strings.NewReader(specJSON))
+	if err != nil {
+		b.Fatalf("Failed to load spec: %v", err)
+	}
+
+	data := map[string]interface{}{
+		"bank_code":      1,
+		"payment_date":   time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+		"payment_amount": 123456.78,
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := p.PackRecord(ctx, data)
+		if err != nil {
+			b.Fatalf("Failed to pack record: %v", err)
+		}
+	}
+}
